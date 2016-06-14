219c8fe58d329304b45f8e4b7241cbd5d1f6d5ce
Corrado Cavalli <corrado@outlook.com>
2016-06-14 22:52:01 +0200

    Updated readme.md

--- Corcav.Behaviors/Properties/AssemblyInfo.cs
+++ Corcav.Behaviors/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Corcav.Behaviors")]
-[assembly: AssemblyCopyright("Copyright ©  2014-2015")]
+[assembly: AssemblyCopyright("Copyright ©  2014-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
--- README.md
+++ README.md
@@ -12,6 +12,7 @@ for more details.
 
 Updates
 =======
+* version 2.2.0 updated to Xamarin Forms v 2.2.0.45 added support to UWP
 * version 2.1.1 updated to Xamarin Forms v 1.4.4.6392
 * version 2.0 RENAMED NAMESPACES TO PREVENT XAMARIN API CONFLICTS, added Unifed API sample and aligned to Xamarin Forms v 1.3.4
 
